6a4474ef3662bd71c0455f49503c6c1c427d1ed1
Rakesh Gummala <rakeshgummala7@gmail.com>
2025-11-28 19:46:25 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import "./index.css";
 import Home from "./pages/Home.jsx";
-import Projects from "./Pages/Projects.jsx";
-import Contact from "./Pages/Contact.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
 
 import Blogs from "./pages/Blogs.jsx";
 import BlogOne from "./pages/blogs/BlogOne.jsx"
